7dde2a1ea9c27aca12a2d640618d23d7452575b0
DESKTOP-J4CVPG5\Antoine <antoine.aubree@protonmail.com>
2021-04-07 11:17:03 +0200

    add project

---
+++ src/main/java/fr/diginamic/klitair/KlitairApplication.java
@@ -0,0 +1,13 @@
+package fr.diginamic.klitair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlitairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlitairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/diginamic/klitair/KlitairApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diginamic.klitair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlitairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
