f0c9d58cf015f5c33ff8c2522d80ee088cae8129
STEPHMC\StephanieMC <stephaniegarcia2302@gmail.com>
2021-04-13 10:57:35 +0200

    Add town_BDD V2

--- src/main/java/fr/diginamic/klitair/controllers/TownController.java
+++ src/main/java/fr/diginamic/klitair/controllers/TownController.java
@@ -1,7 +1,7 @@
 package fr.diginamic.klitair.controllers;
 
 import java.util.List;
-import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,14 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import fr.diginamic.klitair.api.geo.region.RegionDataApi;
 import fr.diginamic.klitair.api.geo.town.TownApiRequest;
-import fr.diginamic.klitair.api.geo.town.TownDataApi;
-import fr.diginamic.klitair.entity.Department;
-import fr.diginamic.klitair.entity.Region;
 import fr.diginamic.klitair.entity.Town;
-import fr.diginamic.klitair.repository.DepartmentRepository;
 import fr.diginamic.klitair.repository.TownRepository;
 import fr.diginamic.klitair.services.TownService;
 
@@ -38,8 +32,7 @@ public class TownController {
 	@Autowired
 	TownApiRequest townApiRequest;
 
-	@Autowired
-	DepartmentRepository departmentRepository;
+	
 
 	@GetMapping("{postCode}")
 	public List<Town> findByPostcode(@PathVariable String postCode) {
@@ -53,26 +46,6 @@ public class TownController {
 
 	}
 
-	@PutMapping
-	public void insertTown() {
-		try {
-			for (TownDataApi townData : townApiRequest.getTownData()) {
-				if (townRepository.findByPostCodes_Code(townData.getCode()) == null) {
-					Town town = new Town();
-					town.setCode(townData.getCode());
-					town.setName(townData.getName());
-					Department department = departmentRepository.findByCode(townData.getCodeDepartement());
-					// population ?
-					if (department != null) {
-						town.setDepartment(department);
-						townRepository.save(town);
-					}
-
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+	
 
 }
--- src/main/java/fr/diginamic/klitair/services/TownService.java
+++ src/main/java/fr/diginamic/klitair/services/TownService.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import fr.diginamic.klitair.api.geo.town.TownApiRequest;
+import fr.diginamic.klitair.api.geo.town.TownDataApi;
+import fr.diginamic.klitair.entity.Department;
 import fr.diginamic.klitair.entity.Town;
+import fr.diginamic.klitair.repository.DepartmentRepository;
 import fr.diginamic.klitair.repository.TownRepository;
 
 /**
@@ -20,28 +24,50 @@ public class TownService {
 
 	@Autowired
 	TownRepository townRepository;
-	
-	@Autowired 
+
+	@Autowired
 	RegionService regionService;
-	
+
 	@Autowired
 	DepartmentService departmentService;
-	
+
 	@Autowired
 	TownApiRequest townApiRequest;
-	
+
+	@Autowired
+	DepartmentRepository departmentRepository;
 
 	public List<Town> findByPostCode(String postCode) {
 		return townRepository.findByPostCodes_Code(postCode);
 	}
 
 	public void update() {
-		
+
 		regionService.insertRegion();
 		departmentService.insertDepartment();
-		
+
 	}
-	
-	
+
+	@PutMapping
+	public void insertTown() {
+		try {
+			for (TownDataApi townData : townApiRequest.getTownData()) {
+				if (townRepository.findByPostCodes_Code(townData.getCode()) == null) {
+					Town town = new Town();
+					town.setCode(townData.getCode());
+					town.setName(townData.getName());
+					Department department = departmentRepository.findByCode(townData.getCodeDepartement());
+					// population ?
+					if (department != null) {
+						town.setDepartment(department);
+						townRepository.save(town);
+					}
+
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
